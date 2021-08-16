49c816ec70144aa982dc4201ae2a3f4935c39b1e
mrabets <newrabgo@gmail.com>
2021-08-16 03:52:17 +0300

    Order posts by date

--- blog_post/blueprint.py
+++ blog_post/blueprint.py
@@ -22,12 +22,6 @@ def add():
 
     return render_template('add.html')
 
-@blog_post.route('/')
-def index():
-    posts = Post.query.all()
-
-    return render_template('index.html', posts=posts)
-
 @blog_post.route('/<int:post_id>')
 def post(post_id):
     post = Post.query.filter_by(id=post_id).one()
--- simple_page/blueprint.py
+++ simple_page/blueprint.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
-
+from models import Post, db
 
 simple_page = Blueprint('simple_page', __name__)
+
+@simple_page.route('/')
+def index():
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
+
+    return render_template('index.html', posts=posts)
 
 @simple_page.route('/about')
 def about():
