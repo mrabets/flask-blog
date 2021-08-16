5df573db7125faa802ca13c868ec07333a12df3e
mrabets <newrabgo@gmail.com>
2021-08-16 23:31:22 +0300

    Create comment model and add one to many

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from sqlalchemy.orm import backref
 from app import db
 
 
@@ -9,10 +11,17 @@ class Post(db.Model):
     author = db.Column(db.String(20))
     date_posted = db.Column(db.DateTime(), default=datetime.now())
     content = db.Column(db.Text)
+    comments = db.relationship('Comment', backref='post')
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    date_commented = db.Column(db.DateTime(), default=datetime.now())
+    comment = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
 
 def init_db():
     db.create_all()
-
 
 if __name__ == '__main__':
     init_db()
